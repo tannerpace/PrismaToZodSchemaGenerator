835a98f7844bbe5a273978afbc19ee6557cab123
TannerB <tannerpace@gmail.com>
2023-08-18 18:22:36 -0400

    never fix

--- prismaToZod.ts
+++ prismaToZod.ts
@@ -10,67 +10,77 @@ function parseEnum(enumStr: string): { name: string, values: string[] } | null {
   return { name, values };
 }
 
-function parseModel(modelStr: string): { name: string, fields: string[] } | null {
+function parseModel(modelStr: string): { name: string, fields: { name: string, type: string }[] } | null {
   const modelMatch = modelStr.match(/model (\w+) {([\s\S]*?)}/);
   if (!modelMatch) return null;
 
   const name = modelMatch[1];
-  const fields = modelMatch[2].trim().split('\n').map(field => field.trim());
+  const fields = modelMatch[2]
+    .trim()
+    .split('\n')
+    .map(field => {
+      const [fieldName, fieldType] = field.trim().split(/\s+/);
+      return { name: fieldName, type: fieldType };
+    });
 
   return { name, fields };
 }
 
-function generateZodSchema(prismaSchema: string) {
+function convertToZod(fieldType: string, enums: string[]): string {
+  switch (fieldType) {
+    case 'String':
+      return 'z.string()';
+    case 'Int':
+      return 'z.number()';
+    case 'Boolean':
+      return 'z.boolean()';
+    default:
+      if (enums.includes(fieldType)) {
+        return `${fieldType}Enum`;
+      }
+      return 'z.unknown()'; // Handle unknown types
+  }
+}
+
+function generateZodSchema(prismaSchema: string): string {
   let zodSchema = "import { z } from 'zod';\n\n";
 
-  // Extract enums and models
-  const enums = prismaSchema.match(/enum \w+ {[\s\S]*?}/g) || [];
-  const models = prismaSchema.match(/model \w+ {[\s\S]*?}/g) || [];
+  const enumMatches = prismaSchema.match(/enum \w+ {[\s\S]*?}/g) || [];
+  const modelMatches = prismaSchema.match(/model \w+ {[\s\S]*?}/g) || [];
 
-  for (const enumStr of enums) {
-    const enumData = parseEnum(enumStr);
-    if (enumData) {
-      zodSchema += `export const ${enumData.name}Enum = z.union([\n  `;
-      zodSchema += enumData.values.map(value => `z.literal('${value}')`).join(',\n  ');
-      zodSchema += '\n]);\n\n';
-    }
-  }
+  const parsedEnums = enumMatches.map(parseEnum).filter(Boolean);
+  const parsedModels = modelMatches.map(parseModel).filter(Boolean);
 
-  for (const modelStr of models) {
-    const modelData = parseModel(modelStr);
-    if (modelData) {
-      zodSchema += `export const ${modelData.name}Schema = z.object({\n`;
-      for (const field of modelData.fields) {
-        const [fieldName, fieldType] = field.split(/\s+/);
-        switch (fieldType) {
-          case 'String':
-            zodSchema += `  ${fieldName}: z.string(),\n`;
-            break;
-          case 'Int':
-            zodSchema += `  ${fieldName}: z.number(),\n`;
-            break;
-          case 'Boolean':
-            zodSchema += `  ${fieldName}: z.boolean(),\n`;
-            break;
-          default:
-            if (enums.includes(fieldType)) {
-              zodSchema += `  ${fieldName}: ${fieldType}Enum,\n`;
-            }
-            break;
-        }
-      }
-      zodSchema += '});\n\n';
-    }
-  }
+  parsedEnums.forEach(enumData => {
+    zodSchema += `export const ${enumData.name}Enum = z.union([\n  `;
+    zodSchema += enumData.values.map(value => `z.literal('${value}')`).join(',\n  ');
+    zodSchema += '\n]);\n\n';
+  });
+
+  parsedModels.forEach(modelData => {
+    zodSchema += `export const ${modelData.name}Schema = z.object({\n`;
+    modelData.fields.forEach(field => {
+      zodSchema += `  ${field.name}: ${convertToZod(field.type, parsedEnums.map(e => e.name))},\n`;
+    });
+    zodSchema += '});\n\n';
+  });
 
   return zodSchema;
 }
 
-const prismaSchemaPath = './schema.prisma';
-const zodSchemaOutputPath = './zodSchemas.ts';
+function main() {
+  const prismaSchemaPath = './schema.prisma';
+  const zodSchemaOutputPath = './zodSchemas.ts';
 
-const prismaSchema = fs.readFileSync(prismaSchemaPath, 'utf8');
-const zodSchema = generateZodSchema(prismaSchema);
+  try {
+    const prismaSchema = fs.readFileSync(prismaSchemaPath, 'utf8');
+    const zodSchema = generateZodSchema(prismaSchema);
 
-fs.writeFileSync(zodSchemaOutputPath, zodSchema);
-console.log(`Zod schema generated as ${zodSchemaOutputPath}`);
+    fs.writeFileSync(zodSchemaOutputPath, zodSchema);
+    console.log(`Zod schema generated as ${zodSchemaOutputPath}`);
+  } catch (error) {
+    console.error("Error generating Zod schema:", error.message);
+  }
+}
+
+main();
