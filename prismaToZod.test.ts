2636056817c322470f61403ada102d32ac1077cf
TannerB <tannerpace@gmail.com>
2023-08-18 23:56:44 -0400

    tests

--- prismaToZod.test.ts
+++ prismaToZod.test.ts
@@ -1,41 +1,60 @@
 const fs = require('fs');
 
-
 import { generateZodSchema, main } from './prismaToZod';
+import { sortZodSchemas } from './utils/helpers';
 import { badPath, goodPath, goodSchema, badSchema } from './testHelpers';
 
+
+jest.mock('fs');
+
 describe('Prisma to Zod Conversion', () => {
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
   beforeAll(() => {
+    // Mock file system functions
+    fs.writeFileSync.mockImplementation(() => { });
+    fs.unlinkSync.mockImplementation(() => { });
+    fs.readFileSync.mockImplementation(path => {
+      if (path === goodPath) return goodSchema;
+      if (path === badPath) return badSchema;
+      return null;
+    });
+
+
     fs.writeFileSync(goodPath, goodSchema);
     fs.writeFileSync(badPath, badSchema);
   });
 
+  afterAll(() => {
 
-
-  describe('generateZodSchema', () => {
-    it('should convert a simple Prisma schema to a Zod schema', () => {
-      const goodSchema = fs.readFileSync(goodPath, 'utf8');
-      const zodSchema = generateZodSchema(goodSchema);
-      expect(zodSchema).toMatchSnapshot();
-    });
-
-    it('should throw an error if the Prisma schema is invalid', () => {
-      const badSchema = fs.readFileSync(badPath, 'utf8');
-      expect(() => generateZodSchema(badSchema)).toThrowErrorMatchingSnapshot();
-    });
+    fs.unlinkSync(goodPath);
+    fs.unlinkSync(badPath);
   });
 
+
+
   describe('main', () => {
-    it('should generate a Zod schema file', () => {
+    afterEach(() => {
+      try {
+        fs.unlinkSync('./testOutput.ts');
+      } catch (error) { }
+    });
+
+    it('should correctly generate a Zod schema file from a valid Prisma schema', () => {
       main(goodPath, './testOutput.ts');
       const zodSchema = fs.readFileSync('./testOutput.ts', 'utf8');
       expect(zodSchema).toMatchSnapshot();
-      fs.unlinkSync('./testOutput.ts');
     });
 
-    it('should throw an error if the Prisma schema is invalid', () => {
-      expect(() => main(badPath, './testOutput.ts')).toThrowErrorMatchingSnapshot();
-    });
+
+    it('expect bad should match good after sorting', () => {
+      const zodSchema = generateZodSchema(badSchema);
+      const sortedZodSchema = sortZodSchemas(zodSchema);
+      expect(sortedZodSchema).toMatchSnapshot();
+    }
+    );
   });
 });
-
