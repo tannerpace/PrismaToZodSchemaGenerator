796370067f05e81d30143eed8e2de31ab1c8bcd9
TannerB <tannerpace@gmail.com>
2023-08-19 02:32:29 -0400

    convert

--- dist/src/prismaToZod.js
+++ dist/src/prismaToZod.js
@@ -5,7 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.main = exports.generateZodSchema = void 0;
 const path_1 = __importDefault(require("path"));
-// ...
 const helpers_1 = require("./utils/helpers");
 function generateZodSchema(prismaSchema) {
     let zodSchema = "import * as z from 'zod';\n\n";
