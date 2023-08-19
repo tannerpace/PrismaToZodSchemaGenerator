5580232ab25618696f17b52a0f29006cadf1957b
TannerB <tannerpace@gmail.com>
2023-08-18 22:03:10 -0400

    clean up

--- prismaToZod.ts
+++ prismaToZod.ts
@@ -1,143 +1,9 @@
 import fs from 'fs';
-
-type Schema = {
-  name: string;
-  fields: { name: string, type: string }[];
-};
-
-type Enum = {
-  name: string;
-  values: string[];
-};
-
-function isNotNull<T>(value: T | null): value is T {
-  return value !== null;
-}
-
-function isError(err: unknown): err is Error {
-  return err instanceof Error;
-}
-
-function parseModel(modelStr: string): Schema | null {
-  const modelMatch = modelStr.match(/model (\w+) {([\s\S]*?)}/);
-  if (!modelMatch) return null;
-
-  const name = modelMatch[1];
-  const fields = modelMatch[2]
-    .trim()
-    .split('\n')
-    .filter(line => !line.trim().startsWith('@') && line.includes(' '))
-    .map(field => {
-      const [fieldName, fieldType] = field.trim().split(/\s+/);
-      if (!fieldName || !fieldType) {
-        console.error(`Parsing error with field: "${field}" in model: ${name}`);
-        return null;
-      }
-      return { name: fieldName, type: fieldType };
-    }).filter(isNotNull);
-
-  return { name, fields };
-}
-
-
-
-
-function parseEnum(enumStr: string): { name: string, values: string[] } | null {
-  const enumMatch = enumStr.match(/enum (\w+) {([\s\S]*?)}/);
-  if (!enumMatch) return null;
-  const name = enumMatch[1];
-  const values = enumMatch[2].trim().split(/\s+/);
-  return { name, values };
-}
-
-function convertToZod(fieldType: string, enums: string[], models: string[]): string {
-  switch (fieldType) {
-    case 'String':
-      return 'z.string()';
-    case 'Int':
-      return 'z.number()';
-    case 'Boolean':
-      return 'z.boolean()';
-    default:
-      if (enums.includes(fieldType)) {
-        return `${fieldType}Enum`;
-      }
-      if (models.includes(fieldType)) {
-        return `${fieldType}Schema`;
-      }
-      return '';
-  }
-}
-
-function sortZodSchemas(zodSchema: string): string {
-  const lines = zodSchema.split('\n');
-
-  // Extract schema names and their content lines
-  const schemaContent: { [name: string]: string[] } = {};
-  let currentName: string | null = null;
-
-  lines.forEach(line => {
-    const match = line.match(/const (\w+)(Enum|Schema) =/);
-    if (match) {
-      currentName = match[1] + (match[2] || '');
-      schemaContent[currentName] = [];
-    }
-
-    if (currentName) {
-      schemaContent[currentName].push(line);
-      if (line.trim().endsWith(');') || line.trim().endsWith(']')) {
-        currentName = null;
-      }
-    }
-  });
-
-  const schemaNames = Object.keys(schemaContent);
-
-  // Create a dependency graph
-  const graph: { [name: string]: string[] } = {};
-
-  schemaNames.forEach(name => {
-    graph[name] = [];
-    schemaNames.forEach(depName => {
-      if (name !== depName && schemaContent[name].some(l => l.includes(depName))) {
-        graph[name].push(depName);
-      }
-    });
-  });
-
-  // Topological sort
-  const result: string[] = [];
-  const visited: { [name: string]: boolean } = {};
-  const visiting: { [name: string]: boolean } = {}; // For detecting cycles
-
-  function visit(name: string) {
-    if (visiting[name]) {
-      throw new Error(`Cyclic dependency detected at ${name}`);
-    }
-
-    if (!visited[name]) {
-      visiting[name] = true;
-      graph[name].forEach(visit);
-      visited[name] = true;
-      result.push(name);
-      delete visiting[name];
-    }
-  }
-
-  schemaNames.forEach(name => {
-    if (!visited[name]) visit(name);
-  });
-
-  return result.map(name => schemaContent[name].join('\n')).join('\n');
-}
-
-
-
+import { convertToZod, isError, isNotNull, parseEnum, parseModel, sortZodSchemas } from './utils/helpers';
 
 
 function generateZodSchema(prismaSchema: string): string {
   let zodSchema = "import * as z from 'zod';\n\n";
-
 
   const enumMatches = prismaSchema.match(/enum \w+ {[\s\S]*?}/g) || [];
   const modelMatches = prismaSchema.match(/model \w+ {[\s\S]*?}/g) || [];
@@ -156,7 +22,7 @@ function generateZodSchema(prismaSchema: string): string {
 
     for (const field of modelData.fields) {
       const fieldType = convertToZod(field.type, parsedEnums.map(e => e.name), parsedModels.map(m => m.name));
-      if (fieldType) {  // Skip unsupported types so we don't get a bunch of `z.unknown()` fields
+      if (fieldType) {
         zodSchema += `  ${field.name}: ${fieldType},\n`;
       }
     }
@@ -171,13 +37,11 @@ function main() {
   try {
     const prismaSchema = fs.readFileSync(prismaSchemaPath, 'utf8');
     const zodSchema = generateZodSchema(prismaSchema);
-    // const sortedZodSchema = sortZodSchemas(zodSchema);
     const sortedZodSchema = "import * as z from 'zod';\n\n" + sortZodSchemas(zodSchema);
     fs.writeFileSync(zodSchemaOutputPath, sortedZodSchema);
     console.log(`Zod schema generated as ${zodSchemaOutputPath}`);
   } catch (error) {
     if (isError(error)) {
-
       console.error("conversion error", error.message);
     } else {
       console.error("An unknown error occurred");
--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,6 +1,13 @@
 
+type Schema = {
+  name: string;
+  fields: { name: string, type: string }[];
+};
 
-
+type Enum = {
+  name: string;
+  values: string[];
+};
 
 function isNotNull<T>(value: T | null): value is T {
   return value !== null;
@@ -9,7 +16,8 @@ function isNotNull<T>(value: T | null): value is T {
 function isError(err: unknown): err is Error {
   return err instanceof Error;
 }
-function parseModel(modelStr: string): { name: string, fields: { name: string, type: string }[] } | null {
+
+function parseModel(modelStr: string): Schema | null {
   const modelMatch = modelStr.match(/model (\w+) {([\s\S]*?)}/);
   if (!modelMatch) return null;
 
@@ -17,25 +25,29 @@ function parseModel(modelStr: string): { name: string, fields: { name: string, t
   const fields = modelMatch[2]
     .trim()
     .split('\n')
+    .filter(line => !line.trim().startsWith('@') && line.includes(' '))
     .map(field => {
       const [fieldName, fieldType] = field.trim().split(/\s+/);
+      if (!fieldName || !fieldType) {
+        console.error(`Parsing error with field: "${field}" in model: ${name}`);
+        return null;
+      }
       return { name: fieldName, type: fieldType };
-    });
+    }).filter(isNotNull);
 
   return { name, fields };
 }
 
 
+
+
 function parseEnum(enumStr: string): { name: string, values: string[] } | null {
   const enumMatch = enumStr.match(/enum (\w+) {([\s\S]*?)}/);
   if (!enumMatch) return null;
-
   const name = enumMatch[1];
   const values = enumMatch[2].trim().split(/\s+/);
-
   return { name, values };
 }
-
 
 function convertToZod(fieldType: string, enums: string[], models: string[]): string {
   switch (fieldType) {
@@ -52,21 +64,72 @@ function convertToZod(fieldType: string, enums: string[], models: string[]): str
       if (models.includes(fieldType)) {
         return `${fieldType}Schema`;
       }
-      return 'z.unknown()';
+      return '';
   }
 }
 
+function sortZodSchemas(zodSchema: string): string {
+  const lines = zodSchema.split('\n');
+  // Extracts schema names and their content lines
+  const schemaContent: { [name: string]: string[] } = {};
+  let currentName: string | null = null;
 
+  lines.forEach(line => {
+    const match = line.match(/const (\w+)(Enum|Schema) =/);
+    if (match) {
+      currentName = match[1] + (match[2] || '');
+      schemaContent[currentName] = [];
+    }
 
+    if (currentName) {
+      schemaContent[currentName].push(line);
+      if (line.trim().endsWith(');') || line.trim().endsWith(']')) {
+        currentName = null;
+      }
+    }
+  });
 
+  const schemaNames = Object.keys(schemaContent);
 
+  // Create a dependency graph
+  const graph: { [name: string]: string[] } = {};
 
+  schemaNames.forEach(name => {
+    graph[name] = [];
+    schemaNames.forEach(depName => {
+      if (name !== depName && schemaContent[name].some(l => l.includes(depName))) {
+        graph[name].push(depName);
+      }
+    });
+  });
 
+  // Topological sort
+  const result: string[] = [];
+  const visited: { [name: string]: boolean } = {};
+  const visiting: { [name: string]: boolean } = {}; // For detecting cycles
 
-export {
-  isNotNull,
-  isError,
-  parseModel,
-  parseEnum,
-  convertToZod,
+  function visit(name: string) {
+    if (visiting[name]) {
+      throw new Error(`Cyclic dependency detected at ${name}`);
+    }
+
+    if (!visited[name]) {
+      visiting[name] = true;
+      graph[name].forEach(visit);
+      visited[name] = true;
+      result.push(name);
+      delete visiting[name];
+    }
+  }
+
+  schemaNames.forEach(name => {
+    if (!visited[name]) visit(name);
+  });
+
+  return result.map(name => schemaContent[name].join('\n')).join('\n');
 }
+
+
+
+
+export { isNotNull, isError, parseModel, parseEnum, convertToZod, sortZodSchemas }
